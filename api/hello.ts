3eb62e4e80e67044b4fe3aab48f7a48b63814a9b
rmatsuda21 <reo.matsuda@gmail.com>
2024-01-03 18:41:24 -0500

    refactor api

--- api/hello.ts
+++ api/hello.ts
@@ -1,3 +1,12 @@
-module.exports = (req, res) => {
-  res.status(200).json({ message: "Hello, world!" });
-};
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  });
+}
