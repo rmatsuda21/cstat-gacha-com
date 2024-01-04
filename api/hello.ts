11175e36773d5182b3f1b31e7ca1eefc0e85d9c9
rmatsuda21 <reo.matsuda@gmail.com>
2024-01-03 19:51:04 -0500

    update api test

--- api/hello.ts
+++ api/hello.ts
@@ -1,9 +1,21 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default function handler(
+export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
+  const mysql = await import("serverless-mysql");
+  const db = mysql.default({
+    config: {
+      host: process.env.MYSQL_HOST,
+      database: process.env.MYSQL_DATABASE,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+    },
+  });
+  await db.connect();
+  const results = await db.query("SELECT * FROM cards");
+
   const { name = "World" } = request.query;
-  response.status(200).json({ test: `Hello ${name}!` });
+  response.status(200).json({ test: `Hello ${name}!`, results });
 }
