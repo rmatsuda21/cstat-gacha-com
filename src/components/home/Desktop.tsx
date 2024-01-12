c0801f01706cfc475bdad4548a28e62d3e5b6db0
rmatsuda21 <reo.matsuda@gmail.com>
2024-01-12 16:11:10 -0600

    add desktop wip message

--- src/components/home/Desktop.tsx
+++ src/components/home/Desktop.tsx
@@ -1,8 +1,18 @@
 export default function Desktop() {
   return (
-    <div>
-      <h1>Desktop</h1>
-      <p>Welcome home!</p>
+    <div
+      style={{
+        width: "100%",
+        height: "100svh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+    >
+      <h1>Desktop Site is WIP</h1>
+      <h3>Please visit this page on a mobile browser 🙏</h3>
     </div>
   );
 }
